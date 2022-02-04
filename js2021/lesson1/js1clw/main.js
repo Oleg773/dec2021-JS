86d5069cb2257fbc12401eaa7231fd1bab71a6a7
Cr1stal <78477887+Oleg773@users.noreply.github.com>
2022-02-04 11:11:13 +0200

    Add files via upload

---
+++ js2021/lesson1/js1clw/main.js
@@ -0,0 +1,68 @@
+//  1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+// let masuv = [1,2,3,4,5,6,7,8,9,10];
+// let result = masuv[0]+masuv[1]+masuv[2]+masuv[3]+masuv[4]+masuv[5]+masuv[6]+masuv[7]+masuv[8]+masuv[9];
+// console.log(result);
+
+// 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+// let book ={
+//     name:'Sherlock',
+//     numberofp: '55',
+//     genre: 'roman'
+// }
+// console.log(book)
+
+// 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+//
+// let book = {
+//     name:'Popa',
+//     numberofp:'129',
+//     genre:'roman',
+//     avtor: 'Meri Popins'
+// }
+// console.log(book)
+
+// 4. Створити маис об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+// let book = [{
+//     name:'Popa',
+//     numberofp:129,
+//     genre:'roman',
+//     avtor: 'Meri Popins'
+// },
+//     {
+//         name:'Pipa',
+//         numberofp: 55,
+//         genre:'shooter',
+//         avtor: 'Mister Jin'
+//     }
+//
+// ]
+// console.log(book[0].name)
+
+//5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+//    Значення площі зберігати в змінній s.
+
+// let height = 23;
+// let width = 10;
+// let s = (height*width);
+// console.log(s);
+
+// 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
+// результат помістіть у змінну v.
+
+// let heightC = 10;
+// let dC = 4;
+// let v = Math.round(Math.PI * ( Math.pow( ( dC / 2 ),2) ) * heightC);
+// console.log(v);
+
+// 7. У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
+// Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
+
+
+// let n = Math.pow(3, 2);
+// let m = Math.pow(4, 2);
+// let k= Math.pow(n+m, 2)
+// console.log(k);
+
+
+
+
